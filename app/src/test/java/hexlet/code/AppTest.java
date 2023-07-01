763fb2af06e2c05e1f21e1ca6a4880d577fbcfc2
exic <ping.u@ya.ru>
2023-07-01 16:48:43 +0300

    Refactoring

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    public void test1(){
+    public void test1() {
         String str = "String";
         assertThat(str).isNotNull();
         System.out.println("Done");
