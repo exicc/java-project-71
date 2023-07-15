d0b1e2c4c0857d0040963448fd7bdb9295183fb2
exic <ping.u@ya.ru>
2023-07-16 01:49:54 +0300

    Some fixes

--- app/src/test/java/hexlet/code/DifferJsonTest.java
+++ app/src/test/java/hexlet/code/DifferJsonTest.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -9,57 +10,48 @@ import java.io.IOException;
 public class DifferJsonTest {
 
     String format = "stylish";
+    @BeforeEach
+    void init() {
+
+    }
     @Test
     void testJsonFormatter() throws IOException, IllegalAccessException {
-        File file1 = createTempFile("""
-                {
-                  "numbers1": [1, 2, 3, 4],
-                  "numbers2": [2, 3, 4, 5],
-                  "default": null,
-                  "numbers3": [3, 4, 5],
-                  "chars1": ["a", "b", "c"],
-                  "chars2": ["d", "e", "f"]
-                }
-                """);
-        File file2 = createTempFile("""
-                {
-                  "numbers1": [1, 2, 3, 4],
-                  "numbers2": [22, 33, 44, 55],
-                  "default": ["value1", "value2"],
-                  "numbers4": [4, 5, 6],
-                  "chars1": ["a", "b", "c"],
-                  "chars2": false,
-                  "obj1": {
-                      "nestedKey": "value",
-                      "isNested": true
-                    }
-                }
-                """);
+
+        String filePath1 = "src/test/resources/complex1.json";
+        String filePath2 = "src/test/resources/complex2.json";
+
+        // Создаем объект File
+        File file1 = new File(filePath1);
+        File file2 = new File(filePath2);
 
         String expectedDiff = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - default: null
-                  + default: [value1, value2]
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey:value, isNested:true}
-                }""";
+               {
+                   chars1: [a, b, c]
+                 - chars2: [d, e, f]
+                 + chars2: false
+                 - checked: false
+                 + checked: true
+                 - default: null
+                 + default: [value1, value2]
+                 - id: 45
+                 + id: null
+                 - key1: value1
+                 + key2: value2
+                   numbers1: [1, 2, 3, 4]
+                 - numbers2: [2, 3, 4, 5]
+                 + numbers2: [22, 33, 44, 55]
+                 - numbers3: [3, 4, 5]
+                 + numbers4: [4, 5, 6]
+                 + obj1: {nestedKey:value, isNested:true}
+                 - setting1: Some value
+                 + setting1: Another value
+                 - setting2: 200
+                 + setting2: 300
+                 - setting3: true
+                 + setting3: none
+               }""";
         String actualDiff = Differ.generate(file1, file2, format);
 
         Assertions.assertEquals(expectedDiff, actualDiff);
-    }
-
-    private File createTempFile(String content) throws IOException {
-        File tempFile = File.createTempFile("test", ".json");
-        tempFile.deleteOnExit();
-
-        java.nio.file.Files.writeString(tempFile.toPath(), content);
-        return tempFile;
     }
 }
